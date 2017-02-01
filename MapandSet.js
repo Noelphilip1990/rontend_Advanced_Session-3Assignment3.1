672c246aabe0dff30d3f85f4ae2d3892c9578436
Noelphilip1990 <noyo1000gt@gmail.com>
2017-02-01 12:06:08 +0530

    Add files via upload
    Assignment 3.2

---
+++ MapandSet.js
@@ -0,0 +1,49 @@
+//Map Example
+var m = new Map();
+m.set(1,"First");
+m.set("2","Second");
+m.set({third:3},"Third");
+
+m.forEach(function(value,key)
+{
+	console.log(`Key : ${key} ,Value: ${value}`);
+});
+
+//Set Example Adding values and Printing them using for Loop
+var s = new Set();
+if(s.has(1)==false)
+{
+s.add(1);
+}
+if(s.has(2)==false)
+	 {
+	 	s.add(2);
+	 }
+	 if(s.has(3)==false)
+	 {
+	 	s.add(3);
+	 }
+	 if(s.has(1)==false)
+	 {
+	 	s.add(1);
+	 }
+	 console.log("Set size: "+s.size);
+	 s.forEach(function(value)
+	 {
+	 	console.log("Values:"+value);
+	 });
+
+
+	 //Array  of String passed Set and string array printed by using spread
+
+	 var str = ["Hello","John","Add","Sheep"];
+	 var s2 = new Set(str);
+	 console.log(s2.has("Hello"));
+	 console.log(s2.has("John"));
+	 console.log(s2.has("Add"));
+	 console.log(s2.has("Sheep"));
+	 function print(...args)
+	 {
+	 	console.log(args);
+	 }
+	 print(...str);
